86cd99b98e9d16668b26310d506253d272a40977
atagadtad <aarontagadtad@gmail.com>
2019-09-18 18:49:51 +0000

    added 1_queries folder and added 6 query files

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,2 +1,11 @@
 INSERT INTO users (name, email, password)
-VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES('Louisa Meyer', jacksonrose@hotmail.com, '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Speed lamp', 'Light. Bright. Lamp', )
